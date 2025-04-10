c5ef59125b4efd8eb45d6251b3a9b0cb927b45bc
Sariyev Maxot <aleksandrlabincev@gmail.com>
2025-04-10 13:15:03 +0500

    Initial commit

---
+++ src/Js/javaScript.js
@@ -0,0 +1,21 @@
+const photoInput = document.getElementById('photoInput');
+const preview = document.getElementById('preview');
+
+photoInput.addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            preview.src = event.target.result;
+        }
+        reader.readAsDataURL(file);
+    }
+});
+const removeH1Button = document.getElementById('photoInput');
+const h1Element = document.getElementById('knopka');
+
+removeH1Button.addEventListener('click', function() {
+    if (h1Element) {
+        h1Element.remove();
+    }
+});
